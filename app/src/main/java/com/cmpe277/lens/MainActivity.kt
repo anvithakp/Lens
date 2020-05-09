34f2fac33e830d7ff47f50543ad708092f5507df
akankshajaiswal8111 <akanksha.jaiswal@sjsu.edu>
2020-05-09 13:03:07 -0700

    camera

--- app/src/main/java/com/cmpe277/lens/MainActivity.kt
+++ app/src/main/java/com/cmpe277/lens/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.cmpe277.lens
 
+
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
-import android.content.Intent
-import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Build
 import android.provider.AlarmClock
+import android.provider.MediaStore
 import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.method.LinkMovementMethod
@@ -15,24 +25,132 @@ import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.cmpe277.lens.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.google.firebase.ml.vision.text.FirebaseVisionText
-import com.cmpe277.lens.databinding.ActivityMainBinding
-import androidx.databinding.DataBindingUtil
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val RC_SIGN_IN = 123
+    private val ODT_PERMISSION_REQUEST = 1234
+    private val ODT_REQUEST_IMAGE_CAPTURE = 12345
+
+    var outputFileUri: Uri? = null
+
     private lateinit var binding : ActivityMainBinding
     lateinit var imageView: ImageView
     lateinit var editText: EditText
+    lateinit var logoutBtn: Button
+    lateinit var cameraBtn: Button
+    lateinit var findTextBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser == null) {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            Toast.makeText(this, "Welcome " + auth.currentUser!!.email, Toast.LENGTH_SHORT).show()
+        }
+
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         imageView = binding.imageView
         editText = binding.editText
+        findTextBtn = binding.findtextBtn
+        logoutBtn = binding.logoutBtn
+        cameraBtn = binding.cameraBtn
+
+        logoutBtn.setOnClickListener {
+            AuthUI.getInstance().signOut(this)
+                .addOnCompleteListener() {
+                    //Toast.makeText(this,"Goodbye "+auth.currentUser!!.email, Toast.LENGTH_SHORT).show()
+                    showSignInOptions()
+                }
+
+                .addOnFailureListener { e ->
+                    Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                }
+        }
+//        val intent = Intent(this, LoginActivity::class.java)
+//        startActivity(intent)
+//        finish()
+//    }
+
+        cameraBtn.setOnClickListener {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_DENIED) {
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED
+
+                    val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    requestPermissions(permission,ODT_PERMISSION_REQUEST)
+
+                }
+                else {
+                    openCamera()
+                }
+            }
+            else {
+                openCamera()
+            }
+        }
+
+}
+    fun showSignInOptions() {
+
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+            .setAvailableProviders(listOf(
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.GoogleBuilder().build()
+            ))
+            .setTheme(R.style.AppTheme)
+            .build(),
+            RC_SIGN_IN )
+
     }
+
+    private fun openCamera() {
+
+        //val packageManager = activity!!.packageManager
+        val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePhotoIntent.resolveActivity(packageManager) != null) {
+            val values = ContentValues()
+            values.put(MediaStore.Images.Media.TITLE, "")
+            outputFileUri = contentResolver
+                .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)!!
+            takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri)
+            startActivityForResult(takePhotoIntent, ODT_REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+        ) {
+            //super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode) {
+            ODT_PERMISSION_REQUEST -> {
+                if(grantResults.size > 0 && grantResults[0]== PackageManager.PERMISSION_GRANTED) {
+                    openCamera()
+                }
+                else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
     fun selectImage(v: View) {
         val intent = Intent()
         intent.type = "image/*"
@@ -44,11 +162,20 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 1 && resultCode == androidx.appcompat.app.AppCompatActivity.RESULT_OK) {
             binding.imageView.setImageURI(data!!.data)
+            findTextBtn.isEnabled = true
 
+
+        } else if(resultCode== Activity.RESULT_OK) {
+            binding.imageView.setImageURI(outputFileUri)
+            findTextBtn.isEnabled = true
         }
+
     }
 
     fun startRecognizing(v: View) {
+        if(v == null) {
+            Toast.makeText(this, "No image selected", Toast.LENGTH_SHORT).show()
+        }
         if (imageView.drawable != null) {
             editText.setText("")
             v.isEnabled = false
@@ -57,14 +184,14 @@ class MainActivity : AppCompatActivity() {
             val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
 
             detector.processImage(image)
-                    .addOnSuccessListener { firebaseVisionText ->
-                        v.isEnabled = true
-                        processResultText(firebaseVisionText)
-                    }
-                    .addOnFailureListener {
-                        v.isEnabled = true
-                        editText.setText("Failed")
-                    }
+                .addOnSuccessListener { firebaseVisionText ->
+                    v.isEnabled = true
+                    processResultText(firebaseVisionText)
+                }
+                .addOnFailureListener {
+                    v.isEnabled = true
+                    editText.setText("Failed")
+                }
         } else {
             Toast.makeText(this, "Select an Image First", Toast.LENGTH_LONG).show()
         }
@@ -104,7 +231,12 @@ class MainActivity : AppCompatActivity() {
             if (toCharArray[i] == '\n') {
                 end = i
                 println("$TAG spanned start  = $start, spanned end = $end")
-                spannableString.setSpan(OnClickListener(this, spannableString.substring(start,end)), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                spannableString.setSpan(
+                    OnClickListener(
+                        this,
+                        spannableString.substring(start, end)
+                    ), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
                 start = end + 1
                 continue
             }
@@ -115,29 +247,6 @@ class MainActivity : AppCompatActivity() {
         editText.movementMethod = LinkMovementMethod.getInstance()
         editText.movementMethod = ScrollingMovementMethod.getInstance()
 
-
-        for (block in resultText.textBlocks) {
-            val blockText = block.text
-            val blockConfidence = block.confidence
-            val blockLanguages = block.recognizedLanguages
-            val blockCornerPoints = block.cornerPoints
-            val blockFrame = block.boundingBox
-            for (line in block.lines) {
-                val lineText = line.text
-                val lineConfidence = line.confidence
-                val lineLanguages = line.recognizedLanguages
-                val lineCornerPoints = line.cornerPoints
-                val lineFrame = line.boundingBox
-                for (element in line.elements) {
-                    val elementText = element.text
-                    val elementConfidence = element.confidence
-                    val elementLanguages = element.recognizedLanguages
-                    val elementCornerPoints = element.cornerPoints
-                    val elementFrame = element.boundingBox
-                   // editText.append(elementText + "\n")
-                }
-            }
-        }
     }
 
     companion object {
