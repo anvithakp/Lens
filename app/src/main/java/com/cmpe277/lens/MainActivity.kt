672e3d73d623f72f92b04d5d37044b10568a2321
anvithakp <anvitha135@gmail.com>
2020-05-16 11:39:38 -0700

    added translate and text-to-speech

--- app/src/main/java/com/cmpe277/lens/MainActivity.kt
+++ app/src/main/java/com/cmpe277/lens/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.cmpe277.lens
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
+import android.os.Build
 import android.os.Bundle
 import android.provider.AlarmClock
+import android.speech.tts.TextToSpeech
 import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.method.LinkMovementMethod
@@ -15,16 +17,29 @@ import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.cmpe277.lens.databinding.ActivityMainBinding
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage
+import com.google.firebase.ml.naturallanguage.languageid.FirebaseLanguageIdentificationOptions
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.google.firebase.ml.vision.text.FirebaseVisionText
-import com.cmpe277.lens.databinding.ActivityMainBinding
-import androidx.databinding.DataBindingUtil
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tts: TextToSpeech
     private lateinit var binding : ActivityMainBinding
     lateinit var imageView: ImageView
     lateinit var editText: EditText
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
@@ -32,6 +47,19 @@ class MainActivity : AppCompatActivity() {
 
         imageView = binding.imageView
         editText = binding.editText
+
+        var listener = TextToSpeech.OnInitListener {
+            if (it == TextToSpeech.SUCCESS) {
+                println("$TAG: Text to speech engine started successfully.")
+                tts.language = Locale.US
+            } else {
+                println("$TAG: Error starting the text to speech engine.")
+            }
+
+        }
+
+    tts = TextToSpeech(this.applicationContext, listener)
+
     }
     fun selectImage(v: View) {
         val intent = Intent()
@@ -71,14 +99,80 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private class OnClickListener(val context: Context, val text: String) : ClickableSpan() {
+    private class OnClickListener(val context: Context, val text: String, val tts: TextToSpeech) :
+        ClickableSpan() {
 
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
         override fun onClick(widget: View) {
-            val intent = Intent(context, WebviewActivity::class.java)
-            intent.putExtra(AlarmClock.EXTRA_MESSAGE, text)
-            context.startActivity(intent)
+            println("$TAG onclick called")
+            Toast.makeText(context, "option 0", Toast.LENGTH_SHORT).show()
+
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Choose an option : ")
+            val options = arrayOf("Search Web", "Speak out loud", "Translate")
+            builder.setItems(options, DialogInterface.OnClickListener { _, which ->
+                when (which) {
+                    0 -> {
+                        val intent = Intent(context, WebviewActivity::class.java)
+                        intent.putExtra(AlarmClock.EXTRA_MESSAGE, text)
+                        context.startActivity(intent)
+                    }
+                    1 -> tts.speak(text, TextToSpeech.QUEUE_ADD, null, "DEFAULT")
+                    2 -> identifyLanguage(text)
+
+                }
+
+            })
+            val dialog = builder.create()
+            dialog.show()
+
+
         }
+
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        private fun identifyLanguage(inputText: String) {
+            val languageIdentification = FirebaseNaturalLanguage
+                .getInstance().getLanguageIdentification(FirebaseLanguageIdentificationOptions.Builder()
+                    .setConfidenceThreshold(0.34f)
+                    .build());
+
+            languageIdentification
+                .identifyLanguage(inputText)
+                .addOnSuccessListener { s ->
+                    println("$TAG, text = $inputText , language = $s")
+                    val sourceLangCode = FirebaseTranslateLanguage.languageForLanguageCode(s)!!
+
+                    val options = FirebaseTranslatorOptions.Builder()
+                        .setSourceLanguage(sourceLangCode)
+                        .setTargetLanguage(FirebaseTranslateLanguage.EN)
+                        .build()
+
+                    val translator = FirebaseNaturalLanguage.getInstance().getTranslator(options)
+
+                        translator.downloadModelIfNeeded().addOnSuccessListener {
+                            println("$TAG, model downloaded ")
+                            translator.translate(inputText)
+                                .addOnSuccessListener { translatedText ->
+                                    println("$TAG, translated text = $translatedText")
+                                    Toast.makeText(context, translatedText, Toast.LENGTH_LONG).show()
+                                    tts.speak(translatedText, TextToSpeech.QUEUE_ADD, null, "DEFAULT")
+                                }
+                                .addOnFailureListener { exception ->
+                                    println("$TAG, translated text failed $exception")
+                                }
+                        }.addOnFailureListener {
+                            println("$TAG, model download exception $it")
+                        }
+
+                }
+                .addOnFailureListener { e ->
+                                println("$TAG, Language identification error : " + e.printStackTrace())
+                                Toast.makeText(context, "error", Toast.LENGTH_SHORT).show()
+                            }
+                }
     }
+
+
 
     private fun processResultText(resultText: FirebaseVisionText) {
         if (resultText.textBlocks.size == 0) {
@@ -104,7 +198,7 @@ class MainActivity : AppCompatActivity() {
             if (toCharArray[i] == '\n') {
                 end = i
                 println("$TAG spanned start  = $start, spanned end = $end")
-                spannableString.setSpan(OnClickListener(this, spannableString.substring(start,end)), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                spannableString.setSpan(OnClickListener(this, spannableString.substring(start,end), tts), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
                 start = end + 1
                 continue
             }
@@ -113,31 +207,31 @@ class MainActivity : AppCompatActivity() {
 
         editText.text = spannableString
         editText.movementMethod = LinkMovementMethod.getInstance()
-        editText.movementMethod = ScrollingMovementMethod.getInstance()
+//        editText.movementMethod = ScrollingMovementMethod.getInstance()
 
 
-        for (block in resultText.textBlocks) {
-            val blockText = block.text
-            val blockConfidence = block.confidence
-            val blockLanguages = block.recognizedLanguages
-            val blockCornerPoints = block.cornerPoints
-            val blockFrame = block.boundingBox
-            for (line in block.lines) {
-                val lineText = line.text
-                val lineConfidence = line.confidence
-                val lineLanguages = line.recognizedLanguages
-                val lineCornerPoints = line.cornerPoints
-                val lineFrame = line.boundingBox
-                for (element in line.elements) {
-                    val elementText = element.text
-                    val elementConfidence = element.confidence
-                    val elementLanguages = element.recognizedLanguages
-                    val elementCornerPoints = element.cornerPoints
-                    val elementFrame = element.boundingBox
-                   // editText.append(elementText + "\n")
-                }
-            }
-        }
+//        for (block in resultText.textBlocks) {
+//            val blockText = block.text
+//            val blockConfidence = block.confidence
+//            val blockLanguages = block.recognizedLanguages
+//            val blockCornerPoints = block.cornerPoints
+//            val blockFrame = block.boundingBox
+//            for (line in block.lines) {
+//                val lineText = line.text
+//                val lineConfidence = line.confidence
+//                val lineLanguages = line.recognizedLanguages
+//                val lineCornerPoints = line.cornerPoints
+//                val lineFrame = line.boundingBox
+//                for (element in line.elements) {
+//                    val elementText = element.text
+//                    val elementConfidence = element.confidence
+//                    val elementLanguages = element.recognizedLanguages
+//                    val elementCornerPoints = element.cornerPoints
+//                    val elementFrame = element.boundingBox
+//                   // editText.append(elementText + "\n")
+//                }
+//            }
+//        }
     }
 
     companion object {
